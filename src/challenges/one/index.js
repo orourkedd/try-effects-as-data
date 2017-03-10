4f0298d0fbb66dd4833a2d2fef7e920632b9651d
psfrankie <frankie-orourke@pluralsight.com>
2017-03-10 11:18:31 -0500

    update challenge one description

--- src/challenges/one/index.js
+++ src/challenges/one/index.js
@@ -2,7 +2,13 @@ import { testIt } from 'effects-as-data/test';
 
 const key = 'one';
 
-const description = 'Let\'s start with something easy.  Return the username from the function.';
+const description = `The spec below is logic-less (notice there are no assertions).  Each effects-as-data spec tests a complete code path through the function.
+
+The first element in the array \`[0, 0]\` represents the first input to the function, the function arguments.  The second element \`[0, 1]\` represents the first output.  This function has only 1 input output pair and, therefore, has only 1 \`[input, output]\` tuple.
+
+Note that most functions have a series of \`[input, ouput]\` tuples.
+
+Let\'s start with something easy.  Return the username from the function.`;
 
 const code = `function * challengeOne ({ username }) {
   return null;
